ff98d0cad44d0ba1b426c40e108d3f134ca703b2
Alexey Kovzel <alexey.kovzel@gmail.com>
2023-07-07 20:34:06 +0200

    Small changes

--- test/SemanticsTest.hs
+++ test/SemanticsTest.hs
@@ -7,7 +7,7 @@ import Runner
 
 
 fib :: Integer -> Integer
-fib n = if n < 2 then 1 else fib (n - 1) + fib (n - 2)
+fib n = if n < 2 then n else fib (n - 1) + fib (n - 2)
 
 fib_test :: Integer -> IO ()
 fib_test arg = putStrLn ("After running function \"fib_iter\" from demo/fib.as with argument " 
