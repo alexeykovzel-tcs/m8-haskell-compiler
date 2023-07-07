23a2fb94839b7578817bd0a3c301b3b93413866f
serkan <s.akin@student.utwente.nl>
2023-07-07 21:15:21 +0200

    semantic tests only working cases

--- test/SemanticsTest.hs
+++ test/SemanticsTest.hs
@@ -2,13 +2,36 @@
 module SemanticsTests where
 
 import Test.QuickCheck
-import Compiler
 import Runner
 
+nums = [1..5]
 
 fib :: Integer -> Integer
 fib n = if n < 2 then n else fib (n - 1) + fib (n - 2)
 
-fib_test :: Integer -> IO ()
-fib_test arg = putStrLn ("After running function \"fib_iter\" from demo/fib.as with argument " 
-                        ++ show arg ++ " the expected result from Sprockell is " ++ show (fib arg) ++ ".") *> runFileFun "demo/fib.as" "fib_iter" [arg]
+fibTestIter :: Integer -> IO ()
+fibTestIter arg = putStrLn ("Running: \"fib_iter (" ++ show arg ++ ")\" \n Expected: " ++ show (fib arg)) *> runFileFun "demo/fib.as" "fib_iter" [arg]
+
+fibTestRec :: Integer -> IO ()
+fibTestRec arg = putStrLn ("Running: \"fib_rec (" ++ show arg ++ ")\" \n Expected: " ++ show (fib arg)) *> runFileFun "demo/fib.as" "fib_rec" [arg]
+
+funTestFive :: IO ()
+funTestFive = putStrLn "Running: \"five ()\" \n Expected: 2, 3, 5" *> runFileFun "demo/functions.as" "five" []
+
+funTestParallel :: IO ()
+funTestParallel = putStrLn "Running: \"parallels.as\" \n Expected: 20000" *> runFile "demo/parallels.as"
+
+runSemanticsTests :: IO ()
+runSemanticsTests =
+    putStrLn "Running SemanticsTests.hs..." *>
+    putStrLn "demo/fib.as: " *>
+    foldr (*>) (pure ()) (map fibTestIter nums) *>
+    putStrLn "Running fib_rec tests..." *>
+    foldr (*>) (pure ()) (map fibTestRec nums) *>
+    putStrLn "demo/functions.as: " *>
+    funTestFive *>
+    putStrLn "demo/parallels.as: " *>
+    funTestParallel *>
+    putStrLn "SemanticsTests.hs finished."
+
+
